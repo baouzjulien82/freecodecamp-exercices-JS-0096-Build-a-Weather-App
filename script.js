5ff46fab2af25949bc0041d20ff05dcce021515e
Julien Baouz <baouzjulien@gmail.com>
2025-12-09 18:32:08 +0100

    Update weather display units in script.js

--- script.js
+++ script.js
@@ -35,9 +35,9 @@ async function showWeather(city) {
     weatherIcon.setAttribute("src", data.weather[0].icon === undefined? "" : data.weather[0].icon);
     mainTemperature.innerHTML = data.main.temp === undefined ? "N/A" : `<span><strong>t°c: </strong>${data.main.temp}</span>`;
     feelsLike.innerHTML = data.main.feels_like === undefined ? "N/A" : `<span><strong>t°c feels like: </strong>${data.main.feels_like}</span>`;
-    humidityDiv.innerHTML = data.main.humidity === undefined ? "N/A" : `<span><strong>humidity: </strong>${data.main.humidity}</span>`;
-    windDiv.innerHTML = data.wind.speed === undefined ? "N/A" : `<span><strong>wind speed: </strong>${data.wind.speed}</span>`;
-    windGust.innerHTML = data.wind.gust === undefined ? "<span><strong>wind gust: </strong>N/A</span>" : `<span><strong>wind gust: </strong>${data.wind.gust}</span>`;
+    humidityDiv.innerHTML = data.main.humidity === undefined ? "N/A" : `<span><strong>humidity \(%\): </strong>${data.main.humidity}</span>`;
+    windDiv.innerHTML = data.wind.speed === undefined ? "N/A" : `<span><strong>wind speed \(m.s\): </strong>${data.wind.speed}</span>`;
+    windGust.innerHTML = data.wind.gust === undefined ? "<span><strong>wind gust \(m.s\): </strong>N/A</span>" : `<span><strong>wind gust: </strong>${data.wind.gust}</span>`;
     weatherMain.innerHTML = data.weather[0].description === undefined ? "N/A" : `<h2>${data.weather[0].description}</h2>`; 
     displayWeatherDiv.style.display = "flex";
 
