5be816e960f2973cf025110e37d61edd5148f62c
Julien Baouz <baouzjulien@gmail.com>
2025-12-09 14:46:06 +0100

    Add displayWeatherDiv for weather display
    Added displayWeatherDiv element to show weather information.

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ const feelsLike = document.getElementById("feels-like");
 const humidityDiv = document.getElementById("humidity");
 const windDiv = document.getElementById("wind");
 const windGust = document.getElementById("wind-gust");
-const weatherMain = document.getElementById("weather-main")
+const weatherMain = document.getElementById("weather-main");
+const displayWeatherDiv = document.getElementById('display-weather-div');
 
 async function getWeather(city) {
   try {
@@ -30,7 +31,15 @@ async function showWeather(city) {
     if (data.error) {
     throw new Error(data.error);
     }
-    
+    locationTitle.innerText = data.name;
+    weatherIcon.setAttribute("src", data.weather[0].icon);
+    mainTemperature.innerText = data.main.temp;
+    feelsLike.innerText = data.main.feels_like;
+    humidityDiv.innerText = data.main.humidity;
+    windDiv.innerText = data.wind.speed;
+    windGust.innerText = data.wind.gust;
+    weatherMain.innerText = data.weather[0].description; 
+    displayWeatherDiv.style.display = "flex";
 
   } catch (error) {
     alert("Something went wrong, please try again later")
