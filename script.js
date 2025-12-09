90f9b75cebc21fb5dc2cfe4abceec6561296d34f
Julien Baouz <baouzjulien@gmail.com>
2025-12-09 12:16:29 +0100

    Add async function to fetch weather data
    Fetch weather data from an API and handle errors.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const weatherData = "https://weather-proxy.freecodecamp.rocks/api/city/<CITY>";
 
+async function getWeather() {
+  try {
+    const response = await fetch(weatherData);
+    const data = await response.json();
+  } catch (error) {
+  console.log(error);
+  }
+}
